8bb0a5f2b50c4103435b130d85e5cd186064a351
Laura Collins <laura.e.collins1@gmail.com>
2020-03-07 10:07:05 -0600

    Add project files.

---
+++ LegoMiniFigs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegoMiniFigs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
